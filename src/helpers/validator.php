111c632e253714011fc1d44ed8f1c31da89d814c
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:19:50 +0200

    Update validator.php

--- src/helpers/validator.php
+++ src/helpers/validator.php
@@ -1,1 +1,17 @@
+<?php
+declare(strict_types=1);
 
+namespace App\Helpers;
+
+class Validator
+{
+    public static function email(string $value): bool
+    {
+        return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public static function required(string $value): bool
+    {
+        return trim($value) !== '';
+    }
+}
