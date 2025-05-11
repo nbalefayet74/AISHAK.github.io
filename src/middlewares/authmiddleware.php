0bc0a49c657eab242763708805d7d93e2e889cc9
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:18:41 +0200

    Update authmiddleware.php

--- src/middlewares/authmiddleware.php
+++ src/middlewares/authmiddleware.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Middlewares;
+
+use App\Services\AuthService;
+
+class AuthMiddleware
+{
+    public function handle(): void
+    {
+        if (!AuthService::user()) {
+            header('Location: /login');
+            exit;
+        }
+    }
+}
 
