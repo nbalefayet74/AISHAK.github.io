1d1af58210711c5910afdbd3c29e17382601117b
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:20:08 +0200

    Update slug.php

--- src/helpers/slug.php
+++ src/helpers/slug.php
@@ -1,1 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+class Slug
+{
+    public static function make(string $text): string
+    {
+        $text = iconv('UTF-8', 'ASCII//TRANSLIT', $text);
+        $text = strtolower(preg_replace('/[^a-z0-9]+/i', '-', $text));
+        return trim($text, '-');
+    }
+}
 
