006764b62c0e149e9dc2197f6497020b9a593525
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:18:58 +0200

    Update csrfmiddleware.php

--- src/middlewares/csrfmiddleware.php
+++ src/middlewares/csrfmiddleware.php
@@ -1,1 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Middlewares;
+
+use function App\Security\csrf_validate;
+
+class CsrfMiddleware
+{
+    public function handle(): void
+    {
+        csrf_validate();   // stoppe la requête si mauvais token
+    }
+}
 
