53e07da10852aefb64bbce76f90f04b8ad12cbdf
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:19:21 +0200

    Update response.php

--- src/helpers/response.php
+++ src/helpers/response.php
@@ -1,1 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Helpers;
+
+class Response
+{
+    public static function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header('Content-Type: application/json');
+        echo json_encode($data, JSON_THROW_ON_ERROR);
+    }
+
+    public static function redirect(string $url): void
+    {
+        header('Location: ' . $url);
+        exit;
+    }
+}
 
