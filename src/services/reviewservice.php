324164badd541dc51f32bc828ea95c076740bd11
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:18:04 +0200

    Update reviewservice.php

--- src/services/reviewservice.php
+++ src/services/reviewservice.php
@@ -1,1 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use function App\Config\db;
+
+class ReviewService
+{
+    public static function create(array $data): void
+    {
+        db()->prepare(
+            'INSERT INTO reviews (ride_id, author_id, rating, comment) 
+                   VALUES (:ride,:author,:rating,:comment)'
+        )->execute([
+            ':ride'    => $data['ride_id'],
+            ':author'  => $data['author_id'],
+            ':rating'  => $data['rating'],
+            ':comment' => $data['comment'],
+        ]);
+    }
+}
 
