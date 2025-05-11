2f90223ab3886e4a5c7967389ccdcd028185a4bd
nbalefayet74 <nathaliebaque74@gmail.com>
2025-05-11 12:20:27 +0200

    Update timestamps.php

--- src/traits/timestamps.php
+++ src/traits/timestamps.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Traits;
+
+trait Timestamps
+{
+    public string $created_at;
+    public string $updated_at;
+
+    public function touch(): void
+    {
+        $now = date('Y-m-d H:i:s');
+        $this->updated_at = $now;
+        if (!isset($this->created_at)) {
+            $this->created_at = $now;
+        }
+    }
+}
 
